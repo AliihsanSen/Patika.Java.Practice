cc22b9a0416606ab08d5c520025c6eae018c087b
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-06 18:42:11 +0300

    06.11.2022

--- src/DeseneGoreMetotOlusturma.java
+++ src/DeseneGoreMetotOlusturma.java
@@ -1,2 +1,19 @@
 public class DeseneGoreMetotOlusturma {
+
+    public static void main(String[] args) {
+
+        String primeNumber = "";
+        for (int i = 1; i <= 100; i++) {
+            int count = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                primeNumber = primeNumber + i + " ";
+            }
+        }
+        System.out.println(primeNumber);
+    }
 }
